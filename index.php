36a3dc9283e21caf284ac00ce7254fbaccbdcc2b
Daniel Arvizu <daniel.m.arvizu@gmail.com>
2024-10-02 18:41:41 -0600

    fixes update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
 define('APP_PATH', ROOT . 'application' . DS);
 
-require_once APP_PATH . 'Config.php';
 require_once APP_PATH . 'Request.php';
 require_once APP_PATH . 'Bootstrap.php';
 require_once APP_PATH . 'Controller.php';
